f98c501dc73fa941f3cd19d43f34638f1abfaa24
MarkSlepter <131391955+MarkSlepter@users.noreply.github.com>
2024-01-24 23:04:30 +0100

    Third commit

--- src/main/java/Calculator/service/OperationService.java
+++ src/main/java/Calculator/service/OperationService.java
@@ -12,31 +12,27 @@ public class OperationService {
             case "sum":
                 double result = operation.getNum1() + operation.getNum2();
                 operation.setResult(result);
-                return operation;
 
             case "sub":
                 double result1 = operation.getNum1() - operation.getNum2();
                 operation.setResult(result1);
-                return operation;
 
             case "mul":
                 double result2 = operation.getNum1() * operation.getNum2();
                 operation.setResult(result2);
-                return operation;
 
             case "div":
                 double result3 = operation.getNum1() / operation.getNum2();
                 operation.setResult(result3);
-                return operation;
 
             case "pow":
                 double result4 = Math.pow(operation.getNum1(), operation.getNum2());
                 operation.setResult(result4);
-                return operation;
+
 
         }
 
-        throw new IllegalArgumentException();
+        return operation;
 
     }
 
